eba3cef3bd2df0a2789b375988ad571f1443338d
jeffreylgoode <jeffreylgoode@gmail.com>
2016-03-20 13:08:18 -0500

    latest fix was to gracefully exit upon ctrl-c

--- gpio.py
+++ gpio.py
@@ -1,14 +1,8 @@
-# my first Raspberry Pi code
-# this is the LED version of hello world
-# I purchased bread board, resistors, jumpers and LEDs from Gigaparts
-
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT) # Green LED
-GPIO.setup (27, GPIO.OUT) # Red LED
-
-# Alternate red and green LEDs on and off, on and off time is 0.5 seconds
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup (27, GPIO.OUT)
 
 while True:
     GPIO.output(17,1)
